f91e5edcd0bce747363e780a66b65d9de293f86b
Jonas <jonas.coding.all@gmail.com>
2026-01-07 23:19:03 +0100

    rough dashboard design

---
+++ src/routes/(authed)/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    let caffineAmount = 0;
+
+    let drinksToday = 0;
+    let drinksThisWeek = 0;
+    let drinksThisMonth = 0;
+    let drinksThisYear = 0;
+</script>
+
+<h1>Dashboard</h1>
+
+<p>{caffineAmount} Caffine today</p>
+<p>{drinksToday} Energy Drinks today</p>
+<p>{drinksThisWeek} Energy Drinks this Week</p>
+<p>{drinksThisMonth} Energy Drinks this Month</p>
+<p>{drinksThisYear} Energy Drinks this Year</p>
+
+<p></p>
